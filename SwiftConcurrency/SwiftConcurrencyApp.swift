37505902bdadbb15105a3ae8318e244b316b3b60
Tirzaan Neil <Tirzaan@tntneil.com>
2025-09-04 20:39:15 -0600

    Initial Commit

---
+++ SwiftConcurrency/SwiftConcurrencyApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftConcurrencyApp.swift
+//  SwiftConcurrency
+//
+//  Created by Tirzaan on 9/4/25.
+//
+
+import SwiftUI
+
+@main
+struct SwiftConcurrencyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
