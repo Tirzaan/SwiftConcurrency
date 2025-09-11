c486c4d1fd0c8e7f9459c9324ce0f9bf215c6173
Tirzaan Neil <Tirzaan@tntneil.com>
2025-09-11 12:44:46 -0700

    [Episode] 3 - Async and Await

--- SwiftConcurrency/ContentView.swift
+++ SwiftConcurrency/ContentView.swift
@@ -13,6 +13,7 @@ struct ContentView: View {
             List {
                 NavigationLink("Lesson 1 - Do, Catch, Try, Throws") { DoCatchTryThrows() }
                 NavigationLink("Lesson 2 - Download Images w/ async") { DownloadImageAsync() }
+                NavigationLink("Lesson 3 - Async/Await") { AsyncAwait() }
             }
             .navigationTitle("Lessons")
         }
